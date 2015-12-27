6799d1dad533efba0ff5dfea6a626c1ac2b95468
XperiAMM <andresmora1676@gmail.com>
2015-12-27 09:55:57 -0600

    Remove Wheatley
    Causes the cell is restarted
    
    Signed-off-by: XperiAMM <andresmora1676@gmail.com>
    
     Cambios para hacer commit:
    	modificado:    arch/arm/configs/maxi_jagnm_defconfig
    	modificado:    drivers/cpufreq/Kconfig
    	modificado:    drivers/cpufreq/Makefile
    	borrado:       drivers/cpufreq/cpufreq_wheatley.c
    	modificado:    include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -401,9 +401,6 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
 extern struct cpufreq_governor cpufreq_gov_ondemandx;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_ondemandx)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
-extern struct cpufreq_governor cpufreq_gov_wheatley;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_wheatley)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_alucard)
